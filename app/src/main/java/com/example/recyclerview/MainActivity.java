6ad97ac7d4c4664758696a0388128b39cb1dfbd1
Sukhovolskij.Andrew <sukhovolskijandrewoid@gmail.com>
2019-10-26 22:18:30 +0300

    Initial commit

---
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.recyclerview;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.recyclerview.adapter.ItemArrayAdapter;
+import com.example.recyclerview.model.DataClass;
+import com.example.recyclerview.model.Item;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    String[] images = DataClass.imageArray;
+
+    // Initializing list view with the custom adapter
+    ArrayList<Item> itemList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initRecyclerView();
+    }
+
+    private void initData() {
+        // Populating list items
+        for (int i = 0; i < 25; i++) {
+            Uri uri = Uri.parse(images[i]);
+            itemList.add(new Item("Name " + i,
+                    "Description " + i + "\n Sample Text"
+                            + "\n Sample Text", uri));
+        }
+    }
+
+    private void initRecyclerView() {
+        ItemArrayAdapter itemArrayAdapter = new ItemArrayAdapter();
+        recyclerView = findViewById(R.id.item_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(itemArrayAdapter);
+        initData();
+        itemArrayAdapter.addItems(itemList);
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener()
+        {
+            @Override
+            public void onScrollStateChanged (@NonNull RecyclerView recyclerView,int newState){
+                super.onScrollStateChanged(recyclerView, newState);
+                //TODO add Log
+            }
+
+            @Override
+            public void onScrolled (@NonNull RecyclerView recyclerView,int dx, int dy){
+                super.onScrolled(recyclerView, dx, dy);
+                //TODO add pagination
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/recyclerview/adapter/ItemArrayAdapter.java
@@ -0,0 +1,56 @@
+package com.example.recyclerview.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.recyclerview.R;
+import com.example.recyclerview.model.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemArrayAdapter extends RecyclerView.Adapter<ViewHolder> {
+    //All methods in this adapter are required for a bare minimum recyclerview adapter
+    private ArrayList<Item> itemList;
+
+    public ItemArrayAdapter() {
+        this.itemList = new ArrayList<>();
+    }
+
+    // specify the row layout file and click for each row
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.list_item, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    // load data in each row element
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        Item item = itemList.get(i);
+        viewHolder.bindData(item);
+    }
+
+    // get the size of the list
+    @Override
+    public int getItemCount() {
+        return itemList == null ? 0 : itemList.size();
+    }
+
+    public void addItems(List<Item> items){
+        if(items == null){
+            return;
+        }else if(items.isEmpty()){
+            return;
+        }else {
+            itemList.addAll(items);
+            //update state of list inside Adapter
+            notifyDataSetChanged();
+        }
+    }
+}
